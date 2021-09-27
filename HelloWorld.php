0d8e5f105dcc6118240caf23977b7f8a9c5ce4d7
kingchetah <saveroaldo@gmail.com>
2021-09-27 09:26:02 +0800

    Upload File PHP

---
+++ HelloWorld.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+</head>
+<body>
+  <?php echo "Aldo Savero";?><br>
+  <?php echo "191081731007";?><br>
+  <?php echo "Hello World, UTS RPL 2021";?><br>
+</body>
+</html>
